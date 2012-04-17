3dd57a154e5ffb8ae1c8f31653d06ad6b1565bae
unknown <calle._.balle@hotmail.com>
2012-04-17 19:45:42 +0200

    Added audio thingies

--- Reports/L3/code/AudioHandler.cpp
+++ Reports/L3/code/AudioHandler.cpp
@@ -1,14 +1,55 @@
-#include <exception>
-using namespace std;
-
 #include "AudioHandler.h"
-#include "BaseScreen.h"
 
-void AudioHandler::PlayGhostAudio() {
-	throw "Not yet implemented";
+AudioHandler::AudioHandler()
+{
+	FMOD::System_Create(&mSystem);
+	
 }
 
-void AudioHandler::PlayCandyAudio() {
-	throw "Not yet implemented";
+
+AudioHandler::~AudioHandler()
+{
+	delete mSystem;
+	delete mSound1;
+	delete mSound2;
+	delete mSound3;
+	delete mChannel1;
+	delete mChannel2;
+	delete mChannel3;
 }
 
+void AudioHandler::Initialize()
+{
+	mSystem->getNumDrivers(&mNumDrivers);
+	mSystem->getDriverCaps(0, &mCaps, 0, &mSpeakerMode);
+	mSystem->setSpeakerMode(mSpeakerMode);
+
+	if (caps & FMOD_CAPS_HARDWARE_EMULATED)
+		mSystem->setDSPBufferSize(1024, 10);
+
+	mSystem->getDriverInfo(0, mName, 256, 0);
+
+	if (strstr(name, "SigmaTel"))   /* Sigmatel sound devices crackle for some reason if the format is PCM 16bit.  PCM floating point output seems to solve it. */
+		mSystem->setSoftwareFormat(48000, FMOD_SOUND_FORMAT_PCMFLOAT, 0,0, FMOD_DSP_RESAMPLER_LINEAR);
+
+    if (!mSystem->init(100, FMOD_INIT_NORMAL, 0))         /* Ok, the speaker mode selected isn't supported by this soundcard.  Switch it back to stereo... */
+    {
+        mSystem->setSpeakerMode(FMOD_SPEAKERMODE_STEREO);
+		mSystem->init(100, FMOD_INIT_NORMAL, 0);/* ... and re-init. */
+    }
+
+	mSystem->set3DSettings(1.0, DISTANCEFACTOR, 1.0f);
+}
+
+void AudioHandler::LoadAudio();
+{
+	mSystem->createSound("../media/drumloop.wav", FMOD_3D, 0, &mSound1);
+    mSound1->set3DMinMaxDistance(0.5f * DISTANCEFACTOR, 5000.0f * DISTANCEFACTOR);
+    mSound1->setMode(FMOD_LOOP_NORMAL);
+
+	mSystem->createSound("../media/jaguar.wav", FMOD_3D, 0, &mSound2);
+    mSound2->set3DMinMaxDistance(0.5f * DISTANCEFACTOR, 5000.0f * DISTANCEFACTOR);
+    mSound2->setMode(FMOD_LOOP_NORMAL);
+
+    mSystem->createSound("../media/swish.wav", FMOD_SOFTWARE | FMOD_2D, 0, &mSound3);
+}
--- Reports/L3/code/AudioHandler.h
+++ Reports/L3/code/AudioHandler.h
@@ -1,21 +1,35 @@
-#include <exception>
-using namespace std;
+#pragma once
 
-#ifndef __AudioHandler_h__
-#define __AudioHandler_h__
+#include "Resources/inc/fmod.hpp"
 
-// #include "BaseScreen.h"
-
-class BaseScreen;
-class AudioHandler;
+#pragma comment (lib, "Resources/lib/fmodex_vc.lib")
 
 class AudioHandler
 {
-	public:
-		BaseScreen* baseScreen;
+public:
+	AudioHandler();
+	~AudioHandler();
 
-		void PlayGhostAudio();
-		void PlayCandyAudio();
-};
+	void Initialise();
+	void LoadAudio();
 
-#endif
+	void PlayGhostAudio();
+	void PlayBackgroundAudio(float x, float y, float z);
+private:
+	FMOD::System*    mSystem;
+    FMOD::Sound*     mSound1;
+	FMOD::Sound*	 mSound2;
+	FMOD::SOUND*	 mSound3;
+    FMOD::Channel*   mChannel1 = 0;
+	FMOD::Channel*	 mChannel2 = 0;
+	FMOD::Channel*   mChannel3 = 0;
+    int              mKey;
+	int				 mNumDrivers;
+    bool             mListenerFlag = true;
+    FMOD_VECTOR      mListenerPos  = { 0.0f, 0.0f, -1.0f * DISTANCEFACTOR };
+    FMOD_SPEAKERMODE mSpeakerMode;
+    FMOD_CAPS        mCaps;
+    char             mName[256];
+private:
+
+}
