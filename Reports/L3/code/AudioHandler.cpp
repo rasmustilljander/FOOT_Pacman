5452216efb64dd2058b1dba0fddab11215ed2d7a
unknown <calle._.balle@hotmail.com>
2012-04-20 14:42:41 +0200

    Updated the audiothingies

--- Reports/L3/code/AudioHandler.cpp
+++ Reports/L3/code/AudioHandler.cpp
@@ -9,9 +9,7 @@ AudioHandler::AudioHandler()
 	mChannel3 = 0;
 
 	mListenerFlag = true;
-
 }
-
 
 AudioHandler::~AudioHandler()
 {
@@ -52,9 +50,7 @@ void AudioHandler::Initialize()
 		mSystem->setDSPBufferSize(1024, 10);
 
 	mSystem->getDriverInfo(0, mName, 256, 0);
-
-	result = mSystem->init(100, FMOD_INIT_NORMAL, 0);
-
+	
 	mListenerPos.x = 0;
 	mListenerPos.y = 0;
 	mListenerPos.z = 0;
@@ -78,43 +74,39 @@ void AudioHandler::LoadAudio()
     mBackgroundSound2->setMode(FMOD_LOOP_NORMAL);
 }
 
-void AudioHandler::UpdatePosition(float x, float y, float z)	//Due to simplicity the naming standard is thrown out of the window here
+void AudioHandler::UpdatePosition(float lPosX, float lPosY, float lPosZ, float lDirX, float lDirY, float lDirZ)
 {
 	FMOD_VECTOR lNewPosition;
 	FMOD_VECTOR lVelocity;
 
-	lNewPosition.x = x * DISTANCEFACTOR;
-	lNewPosition.y = y * DISTANCEFACTOR;
-	lNewPosition.z = z * DISTANCEFACTOR;
-
+	lNewPosition.x = lPosX * DISTANCEFACTOR;
+	lNewPosition.y = lPosY * DISTANCEFACTOR;
+	lNewPosition.z = lPosZ * DISTANCEFACTOR;
+	
 	lVelocity.x = (lNewPosition.x - mListenerPos.x) * (1000 / INTERFACE_UPDATETIME);
     lVelocity.y = (lNewPosition.y - mListenerPos.y) * (1000 / INTERFACE_UPDATETIME);
     lVelocity.z = (lNewPosition.z - mListenerPos.z) * (1000 / INTERFACE_UPDATETIME);
 
 	mListenerPos = lNewPosition;
+	//mListenerDir = lVelocity;
 
 	mSystem->update();
 }
 
 void AudioHandler::PlayBackgroundSound()
 {
-	
     FMOD_VECTOR lPosision = { -10.0f * DISTANCEFACTOR, 0.0f, 0.0f };
-    FMOD_VECTOR lVelocity = {  0.0f, 0.0f, 0.0f };
 
     mSystem->playSound(FMOD_CHANNEL_FREE, mBackgroundSound1, true, &mChannel1);
-    mChannel1->set3DAttributes(&lPosision, &lVelocity);
+    mChannel1->set3DAttributes(&lPosision, &mListenerDir);
     mChannel1->setPaused(false);
 	
 	
     FMOD_VECTOR lPosision2 = { 10.0f * DISTANCEFACTOR, 0.0f, 0.0f };
-    FMOD_VECTOR lVelocity2 = { 0.0f, 0.0f, 0.0f };
 
     mSystem->playSound(FMOD_CHANNEL_FREE, mBackgroundSound2, true, &mChannel2);
-    mChannel2->set3DAttributes(&lPosision2, &lVelocity2);
-    mChannel2->setPaused(false);
-	
-		
+    mChannel2->set3DAttributes(&lPosision2, &mListenerDir);
+    mChannel2->setPaused(false);	
 }
 
 void AudioHandler::StopSound()
@@ -123,8 +115,6 @@ void AudioHandler::StopSound()
 	mChannel2->stop();
 	mChannel3->stop();
 }
-
-
 
 float AudioHandler::getPositionX()
 {
