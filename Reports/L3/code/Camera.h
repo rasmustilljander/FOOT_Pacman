ab7df8ddb72e392c705d3df3889918f6e8ae0f05
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2012-04-30 14:41:11 +0200

    Updated Camera class

--- Reports/L3/code/Camera.h
+++ Reports/L3/code/Camera.h
@@ -1,22 +1,36 @@
 #ifndef CAMERA_H
 #define CAMERA_H
+
 #include "Utilities.h"
+
 class Camera
-{
-	// HEJ
-	
+{	
 public:
 	Camera();
 	virtual ~Camera();
 	void UpdateView();
+
 	void SetPosition(D3DXVECTOR3 lPlayerPos);
+	void Strafe(float d);
+	void Walk(float d);
+	void Pitch(float lAngle);
+	void RotateY(float lAngle);
+
 	void SetLens(float lFieldOfViewY, float lAspect, float lZValueNearPlane, float lZValueFarPlane);
 
 	//Get Functions
-	D3DXMATRIX GetView() const;
-	D3DXMATRIX GetProjection() const;
-	D3DXVECTOR3 GetPosition() const;
-
+	D3DXMATRIX GetView()const
+	{
+		return this -> mView;
+	}
+	D3DXMATRIX GetProjection()const
+	{
+		return this -> mProjection;
+	}
+	D3DXVECTOR3 GetPosition() const
+	{
+		return this -> mPosition;
+	}
 	
 private:	
 	D3DXVECTOR3 mPosition;	//Camera position
