3f1528d606a3464584717d75c725348b49c57a91
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2012-05-03 13:24:09 +0200

    Renamed Waypoints

--- Reports/L3/code/Ghost.h
+++ Reports/L3/code/Ghost.h
@@ -2,21 +2,24 @@
 #define GHOST_H
 
 #include "NonStaticGameObject.h"
+#include "Waypoints.h"
 
 class Ghost: public NonStaticGameObject
 {
 public:
-	Ghost( D3DXVECTOR3 lPosition );
+	Ghost(D3DXVECTOR3 lPosition);
 	virtual ~Ghost();
 
 	void Update(float lDT);
 	void SetTargetPosition(D3DXVECTOR3 lTargetPosition);
+
 private:
 	void SetResources();
 	void SetupVertices();
 
 private:
 	ID3D10ShaderResourceView* mResourceView2;
+
 
 };
 
